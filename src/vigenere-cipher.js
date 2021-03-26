828e52f82d5f984627043f04ab30367da2135e28
DimaKoba <azsx07@rambler.ru>
2021-03-26 17:38:00 +0300

    feat: add solve

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,69 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
+  constructor(direction = true){
+    this.direction = direction
+  }  
+  
+  
+  
+//====================================
+
   encrypt(message, key) {
-  }   
-    
-  decrypt(message, key) {
-    
-      
-  }
+   if(message === undefined || key === undefined){
+     throw new Error();
+   }
+  let messageUpCase = message.toUpperCase();
+  let keyUpCase = key.toUpperCase();
+  let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let resultArr = [];
+
+  for(let i=0, j=0; i<messageUpCase.length; ++i){
+    if(messageUpCase[i] === ' ' || messageUpCase[i] === '!'){
+resultArr.push(messageUpCase[i])
+} else{
+    resultArr.push(abc[(abc.indexOf(messageUpCase[i]) + abc.indexOf(keyUpCase[j++ % keyUpCase.length])) % abc.length])
 }
+}
+return resultArr.join('');
+  
+}
+    //=================================================
+  decrypt(demessage, key) {
+    if(demessage===undefined || key === undefined){
+      throw new Error()
+    }
+    let demessageUpCase = demessage.toUpperCase();
+    let keyUpCase = key.toUpperCase();
+    let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let resultArr = [];
+
+
+    for(let n=0, j=0; n<demessageUpCase.length; ++n){
+      if(demessageUpCase[n] === ' ' || messageUpCase[i] === '!'){
+        resultArr.push(demessageUpCase[n])
+      } else{
+        let result = abc.indexOf(demessageUpCase[n]) - abc.indexOf(keyUpCase[j++ % keyUpCase.length])
+        if(result < 0) {
+          resultArr.push(abc[(result + abc.length) % abc.length])
+        } else {
+          resultArr.push(abc[result]);
+        }
+          
+        }
+      }
+
+      if(direction == false){
+        return resultArr.reverse().join('')
+      } else{
+        return resultArr.join('');
+      }
+    }
+}
+
+
+
+
+  
+
 module.exports = VigenereCipheringMachine;
